3c79748aa4ceb6ccac7cf7995d33d3e8e452e40f
OmarMWarraich <earthling986@gmail.com>
2021-04-27 05:29:16 +0500

    Basic State Example

--- src/App.js
+++ src/App.js
@@ -2,53 +2,39 @@ import React from 'react';
 import './App.css';
 import PropType from 'prop-types';
 
-//Best to use static library when initializing props
-
+//State
+//heart of React component, determines the rendering & behavious of component
+//creates dynamic and interactive components
+//While props from parent are immutable, STATE IS MUTABLE, change in state renders comp at browser
+//Every change in state provide updated data to the user
+//By default State can only be declared for class components ,however, with the advent of React hooks, React State can also be used in functional component.
+//State is mutable only by parent comp which passes state value to its child components as props.
+// Props cannot change as they are immutable, however, a child comp may request parent to make changes
+// Parent component has to pass a function as a props  which will be used by child component to request a state change
+// Once changes are done by parent, react automatically pass updated state value to all the child components where parent comp have passed state values as props
+// This way react insures single source of truth and unidirectional data model
 
 class MyComponent extends React.Component {
-  static propTypes = {
-    cityArray: PropType.array.isRequired
+  state = {
+    cityArray: ['Karachi', 'Lahore', 'Peshawar', 'Quetta']
   }
-  
-  
+
   render () {
-
-    
-    const cityArray = this.props.cityArray
-
-    const shortListedCities = cityArray.filter(city => city.length > 6)
-
-    // this.props.myFunction()
-
     return (
       <ul>
-        {
-          shortListedCities.map(city => <li key={city}>{city}</li>)
-        }
+        {this.state.cityArray.map(city =><li key={city}>{city}</li>)}
       </ul>
     )
   }
+
 }
 
-
-  
-
-
 function App() {
-  const cityArray1 = ['Karachi', 'Lahore', 'Peshawar', 'Quetta']
-  const cityArray2 = ['Hyderabad', 'Islamabad', 'Sawat', 'Gwadar']
-
-
-  // const myFunction = () => alert('Hello World')
-
   return (
     <div>
-    
-    <MyComponent cityArray={cityArray1}/>
-    <MyComponent cityArray={cityArray2}/>
+      <MyComponent />
     </div>
   )
 }
 
-export default App;
-
+export default App
