9e481fa467c0350887bd7640886da3f4409747cf
OmarMWarraich <earthling986@gmail.com>
2021-04-27 05:55:47 +0500

    Add City in State

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
 import React from 'react';
 import './App.css';
-import PropType from 'prop-types';
 
-//Destructure State and Props
+//Changing State
+// To change state we use setState method. No matter how may key value pairs are there, just pass speific key and their new values you want to update
+// Once setState method updates the state it rerenders that component and your compnent gets updated data.
+// setState fn is an asynchronous function
+// Two variations => 
+// Object based approach //\\ this.setState({cityArray: [...this.state.cityArray, value]})) 
+// Funcitonal Approach //\\ this.setState( (prevState) => {cityArray: [...this.state.cityArray]})
 
+function ControlledInputBox (props) {
+  const { handleInputChange, textBoxValue } = props
+  return (
+    <input type='text' value={textBoxValue} onChange={handleInputChange} />
+  )
+}
+function ButtonComponent (props) {
+  const { addCity } = props
+  return (
+    <button onClick={addCity}>Save changes</button>
+  )
+}
 class MyComponent extends React.Component {
   state = {
-    cityArray: ['Karachi', 'Lahore', 'Peshawar', 'Quetta']
+    cityArray: ['Karachi', 'Lahore', 'Peshawar', 'Quetta'],
+    textBoxValue: ''
   }
-
+  handleInputChange = (e) => {
+    const { value } = e.target
+    this.setState({
+      textBoxValue: value
+    })
+  }
+  addCity = (city) => {
+    const { textBoxValue } = this.state
+    
+    this.setState( prevState => ({
+      cityArray: [...prevState.cityArray, textBoxValue],
+      textBoxValue: ''
+    }))
+  }
   render () {
-    const { myFunction } = this.props
-    const { cityArray } = this.state
-
-    myFunction()
-
+    const { cityArray, textBoxValue } = this.state
+    
     return (
+      <>
       <ul>
         {cityArray.map(city =><li key={city}>{city}</li>)}
       </ul>
+      <ControlledInputBox textBoxValue={textBoxValue} handleInputChange={this.handleInputChange}/>
+      <ButtonComponent addCity={() => this.addCity()}/>
+      </>
     )
   }
 
@@ -27,18 +59,13 @@ class MyComponent extends React.Component {
 function MyFunctionalComponent(props) {
   const { heading } = props
   return <h1>{heading}</h1>
-}
-  
-function App() {
-  const myFunction = () => alert('Hello World!')
-
+}  
+function App() {  
   return (
     <div>
       <MyFunctionalComponent heading='Cities List' />
-      <MyComponent myFunction={myFunction} />
+      <MyComponent />
     </div>
   )
-
 }
-
 export default App
