3962ca91611e5a8f90ae1c36489438182f99e7ae
OmarMWarraich <earthling986@gmail.com>
2021-04-27 20:15:02 +0500

    componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,24 +1,161 @@
 
 import React from 'react';
 import './App.css';
+import styled from 'styled-components';
 
-// Constructor
+// ComponentDidMount
 
-class App extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      firstName: 'Aamir'
+
+const Button = styled.button`
+  background-color: white;
+  color: navy;
+  width: 80px;
+  height: 40px;
+  `
+
+const H1 = styled.h1`
+  font-size = 36px;
+  color: Oilive;
+  `
+
+const Button2 =styled(Button)`
+  background-color: silver;
+  `
+function ControlledInputBox (props) {
+  const { handleInputChange, textBoxValue, errorShowFlag } = props
+
+  return (
+    <>
+      <input type="text" value={textBoxValue} onChange={handleInputChange} />
+      {errorShowFlag && <ErrorMessage errorMsg='Please enter city name' />}
+      </>
+   )
+}
+
+function ButtonComponent () {
+  return (
+    <>
+      <Button>Add City</Button>
+      </>
+  )
+}
+
+function ErrorMessage (props) {
+  const { errorMsg } = props
+
+  return <span style={{ color: 'red' }}>{errorMsg}</span>
+}
+
+class MyComponent extends React.Component {
+  state = {
+    cityArray: [],
+    textBoxValue: '',
+    errorShowFlag: false
+  }
+
+  componentDidMount () {
+    /** External API call can be done here and update the state when data recieved */
+    const dataRecievedFromAPICall = ['Karachi', 'Lahore', 'Peshawar', 'Quetta']
+
+    setTimeout(() => {
+      this.setState({
+        cityArray: dataRecievedFromAPICall
+      })
+    }, 1000)
+  }
+  
+  handleInputChange = e => {
+    const { value } = e.target
+    this.setstate({
+      textBoxValue: value,
+      errorShowFlag: false
+    })
+  }
+
+  addCity = () => {
+    const { textBoxValue } = this.state
+
+    this.setState(prevState => ({
+      cityArray: [...prevState.cityArray, textBoxValue],
+      textBoxValue: ''
+    }))
+  }
+
+  removeCity = e => {
+    const { cityArray } = this.state
+    const { value } = e.target
+    const newCityArray = cityArray.filter(city => city !== value)
+
+    this.setState({ 
+      cityArray: newCityArray
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    const { textBoxValue } = this.state
+
+    if (textBoxValue === '') {
+      this.setState({
+        errorShowFlag: true
+      })
+    } else {
+      this.addCity()
     }
-    this.printMessage = this.printMessage.bind(this)
   }
 
-  printMessage () {
-    return <h1> {`${this.state.firstName} ${this.props.message}`} </h1>
+  render() {
+    const {cityArray,textBoxValue, errorShowFlag} = this.state
+    const showSite = cityArray.length !== 0
+
+    return !showSite ? (
+      <ErrorMessage errorMsg='Loading Data...' />
+    ) : (
+      <>
+        <ul style={{ border: '2px dotted navy', padding: '20px'}}>
+          {cityArray.map(city => (
+            <li
+              key={city}
+              className='listItem'
+              style={{ color: cityArray.length < 6 ? 'blue' : 'red' }}
+              >
+                {city}
+                <Button2 value={city} onClick={this.removeCity}>X</Button2>
+              </li>
+          ))}
+        </ul>
+        <form onSubmit={this.handleSubmit}>
+        <ControlledInputBox
+            errorShowFlag={errorShowFlag}
+            textBoxValue={textBoxValue}
+            handleInputChange={this.handleInputChange}
+            />
+            <ButtonComponent />
+            </form>
+            </>
+    )
   }
 
-  render () {
-    return this.printMessage()
+
+}
+
+function MyFunctionalComponent (props) {
+  const { heading } = props
+  return <H1 style={{ textDecoration: 'underline' }}>{heading}</H1>
+}
+
+function App () {
+  const showSite = true
+
+  if (showSite) {
+    return (
+      <div>
+        <MyFunctionalComponent heading='Cities List' />
+        <MyComponent />
+      </div>
+    )
+  } else {
+    return <ErrorMessage errorMsg='Site under Maintenance' />
   }
 }
 
